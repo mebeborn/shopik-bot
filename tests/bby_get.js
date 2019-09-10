71b3afaf1fdbd9020135f0031e6dc6ae8cecd0bc
mebeborn <mebeborn@gmail.com>
2019-09-10 15:38:56 +0300

    fixes

--- tests/bby_get.js
+++ tests/bby_get.js
@@ -11,9 +11,36 @@ describe('Get exact product from BBY', () => {
   })
 });
 
-describe('Get  correct products from BBY', () => {
+describe('Get  avalable products from BBY', () => {
   it('Checks if we get the right products', () => {
     return getProducts('onlineAvailability=true')
       .then(response => expect(response.products).to.be.an('array'));
   })
 });
+
+
+describe('Get  correct products from BBY', () => {
+  it('Checks if we get the right products', () => {
+    const products = [
+      {
+        sku: 1000006,
+        name:
+          'Spy Kids: All the Time in the World [Includes Digital Copy] [Blu-ray] [2011]',
+        description: null,
+        image:
+          'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/1000/1000006_sa.jpg'
+      },
+      {
+        sku: 1001307,
+        name: 'Diary of a Wimpy Kid [DVD] [2010]',
+        description: null,
+        image:
+          'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/1001/1001307_sa.jpg'
+      }];
+
+    getFavorites(products).then(response => {
+      console.log(response);
+    }).catch(err => err)
+
+  })
+})
